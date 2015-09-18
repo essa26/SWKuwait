c056da8ba3b8f79408adcddeca8f6db312d518eb
essaibrahim <essa.ibrahim@gmail.com>
2015-09-18 12:30:06 +0300

    modelling

--- main/models.py
+++ main/models.py
@@ -7,11 +7,35 @@ from django.db import models
 #number_validator = RegexValidator(r'^[0-9]*$','Please type only numbers for your civil ID')
 
 class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    log = models.OneToOneField(Log)
+    name = models.CharField(max_length=255)
+    mobile = models.CharField(max_length=15)
+    emergency1 = models.CharField(max_length=15)
+    emergency2 = models.CharField(max_length=15)
+
+    def __unicode__(self):
+        return self.user.username
 
 
 class Log(models.Model):
     blood_sugar = models.IntegerField()
     date = models.DateTimeField()
     comment = models.TextField(null=True)
-    user = models.ForeignKey(User)  # django stock user?
+    user = models.ForeignKey('main.UserProfile')  # django stock user?
 
+    def __unicode__(self):
+        return self.user.username
+
+
+class DoctorProfile(models.Model):
+    user = models.OneToOneField(User)
+    name = models.CharField(max_length=255)
+    clinic = models.ForeignKey('main.clinic')
+    mobile = models.CharField(max_length=15)
+
+    def __unicode__(self):
+        return self.user.username
+
+
+class Clinic(models.Model):
